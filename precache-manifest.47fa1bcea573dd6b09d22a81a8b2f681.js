511d903f2b20a2e9cb9e544e245d5446a11f5c3e
Andrey <andreytsuzuki@gmail.com>
2020-05-04 22:54:49 -0300

    Updates

---
+++ precache-manifest.47fa1bcea573dd6b09d22a81a8b2f681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7274c992bbe80897485e46da593b6ae2",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "c1c11a0aacfa5bc66fdd",
+    "url": "/calendar/static/css/main.2401e81c.chunk.css"
+  },
+  {
+    "revision": "575660224ccefbcc4fcc",
+    "url": "/calendar/static/js/2.7d858020.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calendar/static/js/2.7d858020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c11a0aacfa5bc66fdd",
+    "url": "/calendar/static/js/main.ffbd898e.chunk.js"
+  },
+  {
+    "revision": "c5a09d6532e78bfec4db",
+    "url": "/calendar/static/js/runtime-main.c90d7201.js"
+  }
+]);
